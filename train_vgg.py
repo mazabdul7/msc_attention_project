78484641060478e5b1eb2bf1ca7493e44fe968f2
mazabdul7 <mazabdul7@outlook.com>
2022-02-14 20:03:45 +0000

    tools updates

--- train_vgg.py
+++ train_vgg.py
@@ -6,7 +6,6 @@ import numpy as np
 from utils.loader import DataLoader
 from utils.tools import test_model
 from utils.data_sampler import CustomDataGenerator
-print(tf.config.list_physical_devices('GPU'))
 
 # Set configs
 img_height = 224
@@ -16,32 +15,30 @@ epochs = 3
 
 # Build our augmentation
 train_datagen = CustomDataGenerator(
-                rescale=1./255,
                 channel_shift_range = 0.2,
                 horizontal_flip=True,
                 validation_split=0.2,
                 preprocessing_function=tf.keras.applications.vgg16.preprocess_input, dtype=tf.float32)
 test_datagen = CustomDataGenerator(
-                rescale=1./255, 
                 preprocessing_function=tf.keras.applications.vgg16.preprocess_input, dtype=tf.float32)
 
 # Load ImageNet dataset with the VGG augmentation
 loader = DataLoader(batch_size, (img_height, img_width))
 train_set, val_set, test_set = loader.load_ds_generator(train_datagen, test_datagen)
-# train_set.set_subsample(['n03873416'], [3/1000]) # No subsampling
-# train_set._set_index_array()
+train_set.set_subsample(['n03873416'], [5/1000]) # No subsampling
+train_set._set_index_array()
 
 # Load VGG-16 with default as we are not transfer learning
 model = tf.keras.applications.vgg16.VGG16(input_shape=(img_height, img_width, 3))
 model.trainable = True
 
 # Compile (not using original e-2 of paper)
-model.compile(optimizer=tf.keras.optimizers.Adam(),
-              loss= tf.keras.losses.SparseCategoricalCrossentropy(),
+model.compile(optimizer=tf.keras.optimizers.Adam(1e-5),
+              loss= tf.keras.losses.CategoricalCrossentropy(),
               metrics=['accuracy'])
 
-# Test current accuracy on test-set
-#test_model(model, test_set)
+# Test current accuracy on test-set (should be 76.3)
+test_model(model, test_set)
 
 # Train for 5 epochs
 model.fit(train_set, validation_data=val_set, epochs=epochs, steps_per_epoch=train_set.n//batch_size, validation_steps=val_set.n//batch_size, verbose=1)
--- utils/tools.py
+++ utils/tools.py
@@ -1,14 +1,12 @@
 import numpy as np
 
 def test_model(model, test_set) -> None:
-    print('Predicting on test-set...')
+    print('\nPredicting on test-set...')
     test_set.reset()
     pred = model.predict(test_set, steps=test_set.n//test_set.batch_size, verbose=1)
-    pred = np.argmax(pred, axis=1)
-    idx_to_cls = {v: k for k, v in test_set.class_indices.items()} # Convert prediction to unique file handlers
-    preds_cls = np.vectorize(idx_to_cls.get)(pred)
-    filenames = [name.split('/')[-2] for name in test_set.filenames] # Get class labels from folder names
+    pred = np.argmax(pred, axis=-1)
+    cls = np.array(test_set.classes)
     
     print('Computing accuracy...')
-    accuracy = sum([preds_cls[i] == filenames[i] for i in range(len(preds_cls))])/test_set.n
+    accuracy = np.sum(pred == cls)/len(pred)
     print(f'Accuracy: {accuracy}')
