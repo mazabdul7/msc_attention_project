6a9bf17d3ae48879f5c72f36d6cda3ef4ca97d78
mazabdul7 <mazabdul7@outlook.com>
2022-06-14 16:50:10 +0100

    var changes

--- utils/configs.py
+++ utils/configs.py
@@ -2,5 +2,6 @@ config = {
     'logs_path' : r'logs',
     'image_net_path' : r'/fast-data22/datasets/ILSVRC/2012/clsloc',
     'train_path' : r'train',
-    'test_path' : r'val_white'
+    'test_path' : r'val_white',
+    'externals' : r'external_files'
 }
